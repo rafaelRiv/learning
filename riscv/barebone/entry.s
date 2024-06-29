2ee97f0e6a7f06d8bd032f354d1740478aba6a5d
Rafael <rafael.prevost.15@gmail.com>
2024-06-29 11:00:24 -0400

    Use zero register and make simple arithmetic

--- riscv/barebone/entry.s
+++ riscv/barebone/entry.s
@@ -2,27 +2,33 @@
 _start:
   lui t0, 0x10000
 
-  andi t1, t1, 0
-  addi t1, t1, 72
+  addi t1, t1, '2'
   sw t1, 0(t0)
 
-  andi t1, t1, 0
-  addi t1, t1, 101
+  addi t1, zero, '+'
   sw t1, 0(t0)
 
-  andi t1, t1, 0
-  addi t1, t1, 108
+  addi t1, zero, '2'
   sw t1, 0(t0)
 
-  andi t1, t1, 0
-  addi t1, t1, 108
+  addi t1, zero, '='
   sw t1, 0(t0)
 
-  andi t1, t1, 0
-  addi t1, t1, 111
+	addi s0, zero,2
+	addi s1, zero,2
+	add s2, s0, s1
+	addi s3, s2, 48
+  sw s3, 0(t0)
+
+  addi t1, zero, '\n'
   sw t1, 0(t0)
 
-  andi t1, t1, 0
-  addi t1, t1, 10
-  sw t1, 0(t0)
+	# exit
+	# Pourquoi ceci termine le programme ?
+	lui a1, 0x100
+	lui a2, 0x5
+	addi a2, a2, 0x555
+	sw a2, 0(a1)
 
+.data
+helloworld: .string "Hello World\n"
