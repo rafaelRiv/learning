be96547d3a2cd35b644360482496732a7d9f1f86
Rafael <rafael.prevost.15@gmail.com>
2024-06-30 09:37:22 -0400

    Add utilities

--- riscv/barebone/entry.s
+++ riscv/barebone/entry.s
@@ -1,8 +1,10 @@
-.global _start
-_start:
+.global start
+
+.text
+start:
   lui t0, 0x10000
 
-  addi t1, t1, '2'
+	addi t1, zero, '2'
   sw t1, 0(t0)
 
   addi t1, zero, '+'
@@ -23,12 +25,5 @@ _start:
   addi t1, zero, '\n'
   sw t1, 0(t0)
 
-	# exit
-	# Pourquoi ceci termine le programme ?
-	lui a1, 0x100
-	lui a2, 0x5
-	addi a2, a2, 0x555
-	sw a2, 0(a1)
-
-.data
-helloworld: .string "Hello World\n"
+	jal halt
+	
