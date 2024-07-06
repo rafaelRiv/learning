0caeef65f1fc9f1c516525eb1ecd91f00b6b47dc
Rafael <rafael.prevost.15@gmail.com>
2024-07-06 06:10:20 -0400

    Add putChar and putStrLn to print any sequences of char

--- riscv/barebone/helloworld/entry.s
+++ riscv/barebone/helloworld/entry.s
@@ -5,45 +5,63 @@
 
 .text
 start:
-  lui t0, 0x10000
+	# The stack grows from bottom to top, so we put the stack pointer
+	# to the very end of the stack range.
+	la sp, _stack_end
 
-	la t1, str # variable *str
-
-	lw t2, 0(t1)
-  sw t2, 0(t0)
-
-	lw t2, 1(t1)
-  sw t2, 0(t0)
-
-	lw t2, 2(t1)
-  sw t2, 0(t0)
-	
-	lw t2, 3(t1)
-  sw t2, 0(t0)
-
-	lw t2, 4(t1)
-  sw t2, 0(t0)
-
-	lw t2, 5(t1)
-  sw t2, 0(t0)
-
-	lw t2, 6(t1)
-  sw t2, 0(t0)
-
-	lw t2, 7(t1)
-  sw t2, 0(t0)
-
-	lw t2, 8(t1)
-  sw t2, 0(t0)
-
-	lw t2, 9(t1)
-  sw t2, 0(t0)
-
-	lw t2, 10(t1)
-  sw t2, 0(t0)
-
-  addi t1, zero, '\n'
-  sw t1, 0(t0)
+	la a0, str
+	jal putStrLn
 
 	jal halt
+
+# putChar : Char -> IO ()
+putChar:
+	addi sp, sp, -4
+	sw	s0,0(sp)
+
+	# TODO: load this address from a constant in .data
+  lui s0, 0x10000
+
+  sw a0, 0(s0)
+
+	lw s0, 0(sp)
+  addi sp,sp, 4
+
+	jr ra
+
+# string -> IO ()
+putStrLn:
+	addi sp, sp, -8
+	sw	s0,4(sp)
+	sw	ra,0(sp)
+
+	add s0, zero, a0
+
+	lw t0, 0(s0)
+	addi t1, zero, 0
+
+	beq t0,t1, donePutStrLn
 	
+	add a0, zero, t0
+	jal putChar
+
+	addi a0, s0, 1
+
+	jal putStrLn
+
+	lw s0, 4(sp)
+	lw ra, 0(sp)
+  addi sp,sp, 8
+
+	jr ra
+
+	donePutStrLn:
+		addi a0, zero, '\n'
+		jal putChar
+
+		lw s0, 4(sp)
+		lw ra, 0(sp)
+  	addi sp,sp, 8
+
+		jr ra
+
--- riscv/barebone/test/entry.s
+++ riscv/barebone/test/entry.s
@@ -1,7 +1,7 @@
 .global start
 
 .data
-	str: .string "2+2="
+str: .string "2+2="
 
 .text
 start:
@@ -9,52 +9,68 @@ start:
 	# to the very end of the stack range.
 	la sp, _stack_end
 
-	addi t0, zero, 0 # variable i
-	addi t1, zero, 4 # length of str
+	la a0, str
+	jal putStrLn
 
-	jal for
+	addi s1, zero,2
+	addi s2, zero,2	
+	add s3, s1, s2
 
-	for:
-		beq t0,t1, done
-		la a0, str # variable *str
-		add a0, a0, t0
-		jal putChar
-		addi t0,t0,1 # i++
-		j for
-	done:
+	addi a0, s3,48
+	jal putChar
 
-#	addi s1, zero,2
-#	addi s2, zero,2
-	
-#	add s3, s1, s2
-#	add a0, zero,s1
-#	jal putChar
-
-  /*addi s1, zero, '\n'
-  sw s1, 0(s0) */
+  addi a0, zero, '\n'
+	jal putChar 
 
 	jal halt
 
 # putChar : Char -> IO ()
 putChar:
-	addi sp, sp, -32
+	addi sp, sp, -4
 	sw	s0,0(sp)
 
 	# TODO: load this address from a constant in .data
   lui s0, 0x10000
-	
-	lw a0, 0(a0)
+
   sw a0, 0(s0)
 
 	lw s0, 0(sp)
-  addi sp,sp, 32
+  addi sp,sp, 4
 
 	jr ra
 
 # string -> IO ()
-#putStrLn:
-#	addi sp, sp, -32
-#	sw	s0,0(sp)
+putStrLn:
+	addi sp, sp, -8
+	sw	s0,4(sp)
+	sw	ra,0(sp)
+
+	add s0, zero, a0
+
+	lw t0, 0(s0)
+	addi t1, zero, 0
+
+	beq t0,t1, donePutStrLn
+	
+	add a0, zero, t0
+	jal putChar
+
+	addi a0, s0, 1
+
+	jal putStrLn
+
+	lw s0, 4(sp)
+	lw ra, 0(sp)
+  addi sp,sp, 8
+
+	jr ra
+
+	donePutStrLn:
+		lw s0, 4(sp)
+		lw ra, 0(sp)
+  	addi sp,sp, 8
+
+		jr ra
 
 
 	
