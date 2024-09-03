230fc7910d5b2d6d25c4ed2bbfaf37ec53dba039
Rafael <rafael.prevost.15@gmail.com>
2024-09-03 14:18:22 -0400

    First opencascade prog

---
+++ opencascade/init/main.cpp
@@ -0,0 +1,32 @@
+/*#include <Standard_TypeDef.hxx>
+#include <Draw_Main.hxx>
+#include <Draw_Interpretor.hxx>
+
+//==============================================================================
+void Draw_InitAppli(Draw_Interpretor& drawInterpretor);
+
+//==============================================================================
+DRAW_MAIN;
+
+//==============================================================================
+void Draw_InitAppli(Draw_Interpretor& drawInterpretor)
+{
+    Draw::Commands(drawInterpretor);
+
+} // Draw_InitAppli() */
+
+#include <Standard_TypeDef.hxx>
+#include <Draw_Main.hxx>
+#include <Draw_Interpretor.hxx>
+
+
+void Draw_InitAppli(Draw_Interpretor& drawInterpretor);
+
+DRAW_MAIN;
+
+void Draw_InitAppli(Draw_Interpretor& drawInterpretor)
+{
+    Draw::Commands(drawInterpretor);
+}
+
+
