463f36b4b4829e57ee1d46c547738b28bf53ed9f
Rafael <rafael.prevost.15@gmail.com>
2024-09-04 08:30:36 -0400

    Clean up

--- opencascade/init/main.cpp
+++ opencascade/init/main.cpp
@@ -1,20 +1,3 @@
-/*#include <Standard_TypeDef.hxx>
-#include <Draw_Main.hxx>
-#include <Draw_Interpretor.hxx>
-
-//==============================================================================
-void Draw_InitAppli(Draw_Interpretor& drawInterpretor);
-
-//==============================================================================
-DRAW_MAIN;
-
-//==============================================================================
-void Draw_InitAppli(Draw_Interpretor& drawInterpretor)
-{
-    Draw::Commands(drawInterpretor);
-
-} // Draw_InitAppli() */
-
 #include <Standard_TypeDef.hxx>
 #include <Draw_Main.hxx>
 #include <Draw_Interpretor.hxx>
