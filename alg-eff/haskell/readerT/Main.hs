7b92790b10be7d319a3c3bdb5c67b4fdc79ff0c3
Rafael <rafael.prevost.15@gmail.com>
2024-09-15 08:13:04 -0400

    Play with readerT pattern

---
+++ alg-eff/haskell/readerT/Main.hs
@@ -0,0 +1,41 @@
+module Main where
+
+{-# LANGUAGE FlexibleContexts #-}
+import Control.Concurrent.Async.Lifted.Safe
+import Control.Monad.Reader
+import Control.Concurrent.STM
+import Say
+
+data Env = Env
+  { envLog :: !(String -> IO ())
+  , envBalance :: !(TVar Int)
+  }
+
+modify :: (MonadReader Env m, MonadIO m)
+       => (Int -> Int)
+       -> m ()
+modify f = do
+  env <- ask
+  liftIO $ atomically $ modifyTVar' (envBalance env) f
+
+logSomething :: (MonadReader Env m, MonadIO m)
+             => String
+             -> m ()
+logSomething msg = do
+  env <- ask
+  liftIO $ envLog env msg
+
+main :: IO ()
+main = do
+  ref <- newTVarIO 4
+  let env = Env
+        { envLog = sayString
+        , envBalance = ref
+        }
+  runReaderT
+    (concurrently
+      (modify (+ 1))
+      (logSomething "Increasing account balance"))
+    env
+  balance <- readTVarIO ref
+  sayString $ "Final balance: " ++ show balance
